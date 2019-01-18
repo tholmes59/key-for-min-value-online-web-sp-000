a290289cbcfbbb47db64e0279483d382cf4be05c
Tom Holmes <tholmes59@gmail.com>
2019-01-18 18:46:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,11 @@
 
 def key_for_min_value(name_hash)
   smallest_value = ""
-  name_hash.each do |key, value|
+  if name_hash == {}
+    false 
+  elsif name_hash.each do |key, value|
+    
+  end
     
 end
 end
